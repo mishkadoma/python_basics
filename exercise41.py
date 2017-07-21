42b08968f72da1179ba2f78a5f781343781613b3
mishkadoma <mixandoma@ya.ru>
2017-07-22 00:38:53 +0500

    correct misspeling

--- exercise41.py
+++ exercise41.py
@@ -52,7 +52,7 @@ def convert(snippet, phrase):
         for word in param_names:
             result = result.replace("@@@", word, 1)
 
-        result.append(result)
+        results.append(result)
 
     return results
 
