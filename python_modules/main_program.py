b71e3f472261dd9884d7505f6f6da5cc70eea5f8
mishkadoma <mixandoma@ya.ru>
2017-07-23 13:50:55 +0500

    init commit

---
+++ python_modules/main_program.py
@@ -0,0 +1,3 @@
+from some_defs import Print_name as name
+
+print name()
---
+++ python_modules/some_defs.py
@@ -0,0 +1,7 @@
+def Plus(a, b):
+    return a + b
+
+def Print_name():
+    print "Enter your name here:"
+    name = raw_input('> ')
+    return name
